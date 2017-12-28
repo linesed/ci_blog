e4b9befc052ceecc541aeb58e33eb21b84a395c8
linesed <1446034180@qq.com>
2017-12-28 17:51:32 +0800

    ci_blog

---
+++ application/controllers/Login.php
@@ -0,0 +1,53 @@
+<?php
+include_once('News.php');
+
+class Login extends News {
+
+
+	public function index()
+	{
+		//echo "blog login";
+		$this->load->view('templates/header');
+		$this->load->view('news/login');
+		$this->load->view('templates/footer');
+		
+	}
+	function check() {
+		$this->load->library('encrypt');  
+		
+        $this -> load -> model('user_test');
+        $user = $this -> user_test -> u_select($_POST['u_name']);
+		//var_dump($user);
+		$passwd = $user[0] -> upawd;
+		$pawd = $this->encrypt->decode($passwd);
+		
+        if ($user) {
+            if ($pawd == $_POST['u_pawd']) {
+                //echo 'pw right';
+				echo "<script>window.location.href='".site_url('news/')."'</script>";
+                $this -> load -> library('session');
+                $arr = array('s_id' => $user[0] -> uid);
+                $this -> session -> set_userdata($arr);
+            } else {
+                echo 'pw wrong';
+            }
+        } else {
+            echo 'name wrong';
+        }
+    }
+
+    function is_login() {
+        $this -> load -> library('session');
+        if ($this -> session -> userdata('s_id')) {
+            echo "logined";
+        } else {
+            echo "no login";
+        }
+    }
+
+    function logout() {
+        $this -> load -> library('session');
+        $this -> session -> unset_userdata('s_id');
+    }
+
+}
---
+++ application/controllers/News.php
@@ -0,0 +1,112 @@
+<?php
+class News extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('news_model');
+        $this->load->helper('url_helper');
+    }
+
+    public function index()
+    {
+		$this->load->library('calendar');  //加载日历类
+
+		parse_str($_SERVER['QUERY_STRING'], $_GET);
+
+		$this->load->library('pagination'); //加载分页类
+		$this->load->model('news_model');   //加载books模型
+		$res = $this->db->get('myblog');    //进行一次查询            
+		$config['base_url'] = base_url().'index.php/news/index';    //设置分页的url路径              
+		$config['total_rows'] = $res->num_rows();    //得到数据库中的记录的总条数             
+		$config['per_page'] = '3';   //每页记录数
+		$config['prev_link'] = 'Previous ';
+		$config['next_link'] = ' Next';
+		
+		$this->pagination->initialize($config);//分页的初始化
+		
+		if (!empty($_GET['key'])) {
+			$key = $_GET['key'];
+			$w = " title like '%$key%'";
+			
+		}else{
+			$w=1;
+			
+		}
+		
+		$data['blogs'] = $this->news_model->blogs($w,$config['per_page'],$this->uri->segment(3));//得到数据库记录
+		
+		$this->load->view('templates/header');
+		$this->load->view('news/index', $data);
+		$this->load->view('templates/footer');
+		
+    }
+
+    public function view($id = NULL)
+	{
+		$this->load->library('calendar'); 
+
+		$data['blogs_item'] = $this->news_model->up_blogs($id);
+
+		if (empty($data['blogs_item']))
+		{
+			show_404();
+		}
+
+		$data['title'] = $data['blogs_item']['title'];
+		
+
+		$this->load->view('templates/header');
+		$this->load->view('./news/view', $data);
+		$this->load->view('templates/footer');
+	}
+	
+	public function del($id = NULL)
+	{
+		$this->news_model->del_blogs($id);
+
+		//通过js跳回原页面
+		echo'
+			<script language="javascript"> 
+				alert("create success!"); 
+				window.location.href="http://localhost/CI_blog/index.php/news/"; 
+			</script> ';		
+	}
+	
+
+	public function create()
+	{
+		$this->load->library('calendar'); //加载日历类
+		
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$data['title'] = 'Create a news item';
+
+		$this->form_validation->set_rules('title', 'Title', 'required');
+		$this->form_validation->set_rules('text', 'Text', 'required');
+
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('templates/header', $data);
+			$this->load->view('news/create');
+			$this->load->view('templates/footer');
+
+		}
+		else
+		{
+			$this->news_model->add_blogs();
+			
+			//跳回blog添加页面
+			echo'
+			<script language="javascript"> 
+				alert("create success!"); 
+				window.location.href="http://localhost/CI_blog/index.php/news/create"; 
+			</script> ';
+			
+			//$this->load->view('news');
+		}
+		
+	}
+
+}
